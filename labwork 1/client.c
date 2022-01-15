00bc3a176e8d1dbe9b2fd11a93ce5c2273677f51
Nghia <abc@gmail.com>
2022-01-15 21:56:57 +0700

    labwork 1 update

---
+++ labwork 1/client.c
@@ -0,0 +1,49 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+void send_file(int serv, char* s, int sz, char* serverFileName, char* clientFileName){
+    write(serv, serverFileName, strlen(serverFileName));
+
+    // delay
+    for{int i = 0; i<9999; i++};
+    printf("file transfer will start in a minute.....");
+
+    int file = open(clientFileName, O_RDWR);
+
+    int len = 0;
+    while ((len = read(file, s, sz)) > 0) {
+        write (serv, s, len);
+    }
+    close(file);
+}
+
+int main(int argc, char* argv[]) {
+    int so;
+    char s[100];
+    struct sockaddr_in ad;
+
+    socklen_t ad_length = sizeof(ad);
+    struct hostent* hep;
+
+    // create socket
+    int serv = socket(AF_INET, SOCK_STREAM, 0);
+
+    // init address
+    hep = gethostbyname(argv[1]);
+    memset(&ad, 0, sizeof(ad));
+    ad.sin_family = AF_INET;
+    ad.sin_addr = *(struct in_addr *)hep -> h_addr_list[0];
+    ad.sin_port = htons(12345);
+
+    // connect to server
+    connect(serv, (struct sockaddr*)&ad, ad_length);
+
+    // server filename: argv[3]
+    // client filename: argv[2]
+    send_file(serv, s, sizeof(s), argv[3], argv[2]);
+}
---
+++ labwork 1/server.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <fcntl.h>
+
+void hadle_file( int cli, char* s, int sz) {
+
+    memset(s, 0, sz);
+
+    char filename[100];
+    int len = read(cli, filename, sizeof(filename));
+    printf("recelve file: %s", filename);
+
+    int file = open(filename, O_RDWR);
+
+    while((len = read(cli, s, sz)) > 0) {
+        write(file, s, sz);
+    }
+    
+    close(file);
+}
+
+int main() {
+    int ss, cli, pid;
+    struct sockaddr_in ad;
+    char s[100];
+    socklen_t ad_length = sizeof(ad);
+
+    // create the socket
+    ss = socket(AF_INET, SOCK_STREAM, 0);
+
+    // bind the socket to port 12345
+    memset(&ad, 0, sizeof(ad));
+    ad.sin_family = AF_INET;
+    ad.sin_addr.s_addr = INADDR_ANY;
+    ad.sin_port = htons(12345);
+    bind(ss, (struct sockaddr *)&ad, ad_length);
+
+    //listen
+    listen(ss, 0);
+
+    while (1) {
+        // an incoming connection
+        cli = accept(ss, (struct sockaddr *)&ad, &ad_length);
+
+        pid = fork();
+        if (pid == 0) {
+            // I'm the son, I'll serve this client
+            printf("client connected\n");
+            hadle_file(cli, s, sizeof(s));
+            return 0;
+        }
+        else {
+            // I'm the father, continue the loop to accept more clients
+            continue;
+        }
+    }
+    // disconnect
+    close(cli);
+
+}
